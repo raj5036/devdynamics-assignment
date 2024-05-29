d88f15c5b9d513304ff4e9b1094388b56dec5a6b
Raj Karmakar <97rajmath@gmail.com>
2024-05-29 22:35:08 +0530

    Use Select component to display data based on Selected person

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
 import React,{ useState, useEffect } from 'react'
+import Select from 'react-select'
 import { MockAPI } from './lib/ApiClient'
 import './App.css'
 
 function App() {
-  const [appData, setAppData] = useState<unknown>(null)
+  const [appData, setAppData] = useState<any>(null)
 
   useEffect(() => {
     MockAPI()
-      .then(response => {
+      .then((response: any) => {
         console.log(response)
-        setAppData(appData)
+        setAppData(response)
       })
       .catch(error => {
         console.error('API call error', error)
       })
-  }, [])
+  }, [appData])
 
   return (
     <React.Fragment>
       <h1 className='header'>Discover your team highlights</h1>
+      
+      {appData &&
+      <div className='mainContainer'>
+        <Select options={appData.data.AuthorWorklog.rows.map((row: any) => {
+          return {
+            label: row.name, value: row.name
+          }
+        })}/>
+        
+        <div className='totalActivityContainer'>
+          {/* {appData.data.AuthorWorklog.rows} */}
+        </div>
+      </div>
+      }
     </React.Fragment>
   )
 }
