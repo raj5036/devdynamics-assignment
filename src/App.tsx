96ae6b12030e12b90d135e29175123c10bf28d59
Raj Karmakar <97rajmath@gmail.com>
2024-05-30 20:04:01 +0530

    Created onSelectChange handler for chart parameters changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { MockAPI } from './lib/ApiClient'
 import { Developer } from './lib/Types'
 import { ActivityTypes } from './lib/Utils'
 import ActivityItem from './components/ActivityItem/ActivityItem'
+import BarChart from './components/BarChart/BarChart'
 import './App.css'
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
       return {name: activity, value: 0}
     })
   })
+  const [chartParameters, setChartParameters] = useState<Array<any>>([])
 
   useEffect(() => {
     MockAPI()
@@ -43,36 +45,56 @@ function App() {
     })
   }
 
+  const onChartSelectChange = (options: any) => {
+    console.log(options)
+    setChartParameters(options)
+  }
+
   return (
     <React.Fragment>
       <h1 className='header'>Discover your team highlights</h1>
       
       {appData &&
-      <div className='mainContainer'>
-        <div className='selectContainer'>
-          <span>Choose a developer</span>
-          <Select 
-            className='select'
-            options={appData.data.AuthorWorklog.rows.map((row: any) => {
-              return {
-                label: row.name, value: row.name
-              }
-            })}
-            isMulti={false}
-            onChange={onSelectChange}
-          />
-        </div>
-        
-        <div className='totalActivityContainer'>
-          {currentDev.totalActivity.map((activity: any, index: number) => {
-            return <ActivityItem
-              key={index}
-              title={activity.name}
-              value={activity.value}
+        <div className='mainContainer'>
+          <div className='selectContainer'>
+            <span>Choose a developer</span>
+            <Select 
+              className='selectDev'
+              options={appData.data.AuthorWorklog.rows.map((row: any) => {
+                return {
+                  label: row.name, value: row.name
+                }
+              })}
+              isMulti={false}
+              onChange={onSelectChange}
             />
-          })}
+          </div>
+          
+          <div className='totalActivityContainer'>
+            {currentDev.totalActivity.map((activity: any, index: number) => {
+              return <ActivityItem
+                key={index}
+                title={activity.name}
+                value={activity.value}
+              />
+            })}
+          </div>
+
+          <div className='chartContainer'>
+            <Select
+              className='chartSelect'
+              options={ActivityTypes.map((type: string) => {
+                return {
+                  label: type, value: type
+                }
+              })}
+              isMulti={true}
+              onChange={onChartSelectChange}
+              placeholder='Choose parameters'
+            />
+            <BarChart/>
+          </div>
         </div>
-      </div>
       }
     </React.Fragment>
   )
